5fbb43465800bfd9c0e447efe3be1cd54dfe8d42
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:55:22 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_dsc_nodeconfigurations" {
+  description = "All automation_dsc_nodeconfiguration resources"
+  value       = azurerm_automation_dsc_nodeconfiguration.automation_dsc_nodeconfigurations
+}
+
